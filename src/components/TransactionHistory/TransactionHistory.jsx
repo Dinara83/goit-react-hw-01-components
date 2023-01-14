e5c46d107b17c8454a52fde59b9eb41dce847895
Dinara83 <mavleeva.dinara@gmail.com>
2023-01-15 00:20:06 +0200

    formatter

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,30 +1,30 @@
-import css from './TransactionHistory.module.css'
+import css from './TransactionHistory.module.css';
 
 const TransactionHistory = () => {
-    return (
-        <table className={css.transactionHistory}>
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
 
-  <tbody>
-    <tr>
-      <td>Invoice</td>
-      <td>125</td>
-      <td>USD</td>
-    </tr>
-    <tr>
-      <td>Withdrawal</td>
-      <td>85</td>
-      <td>USD</td>
-    </tr>
-  </tbody>
-</table>
-    )
-}
+      <tbody>
+        <tr>
+          <td>Invoice</td>
+          <td>125</td>
+          <td>USD</td>
+        </tr>
+        <tr>
+          <td>Withdrawal</td>
+          <td>85</td>
+          <td>USD</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
 export default TransactionHistory;
